616e167543e7f43495cd86a22c3b29b029994708
Hamish Burnett <n11027401@qut.edu.au>
2025-09-01 16:25:41 +1000

    Update axiosConfig.jsx

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001',
-  baseURL: 'http://3.107.57.56:5001'
+  baseURL: 'http://localhost:5001',
+  //baseURL: 'http://3.107.57.56:5001'
 });
 
 axiosInstance.interceptors.request.use(
